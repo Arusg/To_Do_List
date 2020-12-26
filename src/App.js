cb767da1ce1fd599d3724a2f0a8cff8028874757
Your Name <gevorgyanarus@gmail.com>
2020-12-26 22:38:13 +0400

    homework9

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import React from 'react';
 import './App.css';
-import Header from './Header';
 import Product from './Product';
+import ToDo from './components/toDoList';
+
 
 function App() {
   return (
     <div className="App">
+      <ToDo />
       <Product 
         name="bananas" 
         price="1 $" 
         description="Fresh bananas from Ecuador" 
       />
-      <Header />
+      
      
     </div>
   );
---
+++ src/components/toDoList.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './style.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+
+export default class Todo extends Component {
+    
+        state = {
+            tasks: [],
+            inputValue: ''
+        }
+
+        handleChange = (event) => {
+            this.setState({
+                inputValue: event.target.value
+            });
+        };
+        addNewTask = () => {
+            const inputValue = this.state.inputValue.trim();
+
+            if (!inputValue) {
+                return;
+            }
+
+            const tasks = [...this.state.tasks, inputValue];
+
+
+            this.setState({
+                tasks: tasks,
+                inputValue: ''
+            });
+        }
+
+        removeTask = (event) => {
+           
+           this.setState({
+               tasks: [],
+               
+          }); 
+        }
+
+        render(){
+            const { tasks, inputValue } = this.state;
+
+            const taskComponents = tasks.map((task, index) => {
+                return <li key={index}>
+                            <div>
+                                <h5>{task}</h5>
+                                <i className='fa fa-trash' onClick={this.removeTask}></i>
+     
+                            </div>
+                       </li>
+            });
+            return (
+                <div className= "task">
+                    <h2>ToDo List</h2>
+                    <input value={inputValue} type="text" onChange={this.handleChange} />
+                    <button onClick={this.addNewTask}>Add task</button>
+                    <ol className="card">{taskComponents}</ol>
+                </div>
+            );
+        }
+    }
