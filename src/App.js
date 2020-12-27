4cdb777f8f22992bb59affc9bf572d290c367414
Your Name <gevorgyanarus@gmail.com>
2020-12-27 16:41:29 +0400

    homework9

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './App.css';
 import Product from './Product';
-import ToDo from './components/toDoList';
+import ToDo from './components/ToDoList';
 
 
 function App() {
   return (
     <div className="App">
-      <ToDo />
+    <ToDo />
       <Product 
         name="bananas" 
         price="1 $" 
         description="Fresh bananas from Ecuador" 
       />
+      
       
      
     </div>
