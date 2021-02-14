110e6f017ec334e53ee91575b763f80e66c52bee
Your Name <gevorgyanarus@gmail.com>
2021-02-14 18:04:49 +0400

    Using refs and sending contact form

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -76,9 +76,10 @@ export default function Contact(){
                         else {
                             throw new Error('Something went wrong!');
                         }
+                        
                     }
 
-                    console.log('Form sent successfully');
+                    
                     if (response.status >= 200 && response.status <= 300){
                     setValues({
                         name: '',
