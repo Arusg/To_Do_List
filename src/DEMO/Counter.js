e338357bba422a4bdba9ff07f8bc164a58514527
Your Name <gevorgyanarus@gmail.com>
2021-02-21 12:49:25 +0400

    create counter

---
+++ src/DEMO/Counter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SetCount from './SetCount';
+import ShowCount from './ShowCount';
+
+function Counter(){
+
+    return(
+        <div>
+        <ShowCount/>
+        <SetCount/>
+        </div>
+    )
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Counter from './DEMO/Counter';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
 
+const defaultState = { 
+    count: 0
+};
+export default function reducer(state=defaultState, action){
+    
+    switch(action.type){
+      case 'INCREMENT':{
+        return {
+          ...state,
+          count: state.count+1
+        };
+      }
+      case 'DECREMENT':{
+        return {
+          ...state,
+          count: state.count-1
+        };
+      }
+      default: return state;
+    }
+  }
+
+    const store  = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Counter />
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
